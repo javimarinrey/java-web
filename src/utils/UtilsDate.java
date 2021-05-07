9f807bbcd953ed7dce62904a681395ec07e205cd
jmr <javier.marin@hotusa.com>
2021-05-07 12:28:29 +0200

    commit

--- src/utils/UtilsDate.java
+++ src/utils/UtilsDate.java
@@ -1,5 +1,11 @@
 package utils;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.IsoFields;
+import java.time.temporal.TemporalAdjusters;
+
 public class UtilsDate {
 
     public static String formatTime(long miliseconds) {
@@ -8,6 +14,21 @@ public class UtilsDate {
         int min = (total_seconds / 60) % 60;
         int hours = (total_seconds / 60) / 60;
         return hours + "h:" + min + "m:" + sec + "s";
+    }
+
+    /**
+     *
+     * @param year
+     * @param week
+     * @param dayOfWeek { 1: MONDAY, ..., 7:SUNDAY }
+     * @return
+     */
+    public static String getDateFromWeekYear(int year, int week, int dayOfWeek) {
+        LocalDate localDate = LocalDate.ofYearDay(year, week)
+                .with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, week)
+                .with(TemporalAdjusters.previousOrSame(DayOfWeek.of(dayOfWeek)));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return localDate.format(formatter);
     }
 
 }
